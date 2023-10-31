805968064f93a610e6f7b20c6a7f18fc951aba62
poravoz <116353344+poravoz@users.noreply.github.com>
2023-10-31 15:32:03 +0200

    31.10.2023

--- src/front-end/assets/js/script.js
+++ src/front-end/assets/js/script.js
@@ -1,3 +1,15 @@
-document.getElementById('add-to-cart').addEventListener('click', function() {
-    document.getElementById('cart-marker').style.display = 'inline-block';
+document.addEventListener('DOMContentLoaded', function() {
+    var cartMarker = localStorage.getItem('cartMarker');
+
+    if (cartMarker) {
+        document.getElementById('cart-marker').textContent = cartMarker;
+        document.getElementById('cart-marker').style.display = 'inline-block';
+    }
+    
+    document.getElementById('add-to-cart').addEventListener('click', function() {
+        document.getElementById('cart-marker').textContent = '1';
+        document.getElementById('cart-marker').style.display = 'inline-block';
+
+        localStorage.setItem('cartMarker', '1');
+    });
 });
