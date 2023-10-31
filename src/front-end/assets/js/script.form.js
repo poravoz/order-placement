2f6f3be12a9ae0e3acb49b30b79a234150560798
poravoz <116353344+poravoz@users.noreply.github.com>
2023-10-31 16:50:12 +0200

    31.10.2023

--- src/front-end/assets/js/script.form.js
+++ src/front-end/assets/js/script.form.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     var cartItemsDiv = document.getElementById('cart-items');
     var cartProduct = localStorage.getItem('cartProduct');
-    
+    var formContainer = document.querySelector('.form-container');
+
     if (cartProduct) {
         cartItemsDiv.innerHTML = '<h3>Товар в кошику:</h3><p>' + cartProduct + '</p>';
     } else {
@@ -13,5 +14,6 @@ document.addEventListener('DOMContentLoaded', function() {
         localStorage.removeItem('cartProduct');
         cartItemsDiv.innerHTML = '<p>Кошик пустий</p>';
         parent.postMessage('clearCart', '*');
+        formContainer.style.display = 'none'; 
     });
 });
